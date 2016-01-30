64e7dd31b8dd06920b6edcb84574c253a87f05a8
Keith Salisbury <keith.salisbury@alliants.com>
2016-01-30 22:47:10 +0000

    better sounds

--- web/static/js/sound.js
+++ web/static/js/sound.js
@@ -1,11 +1,16 @@
 export class Sound {
   constructor() {
 
-    this.beep = new Howl({src: ["sounds/beep.wav"]});
+    this.beep = new Howl({
+      src: ["sounds/beep.wav"]
+    });
+    this.laser = new Howl({
+      src: ["sounds/laser-01.wav", "sounds/laser-01.ogg"]
+    });
     this.button = new Howl({src: ["sounds/button-09.wav"]});
   }
 
   new_message() {
-    this.button.play();
+    this.laser.play();
   }
 }
