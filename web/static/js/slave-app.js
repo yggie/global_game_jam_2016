6c872ffa2fe752a71d9bff377bf0442fb4b3842b
Keith Salisbury <keith.salisbury@alliants.com>
2016-01-31 01:08:20 +0000

    player upgrade

--- web/static/js/slave-app.js
+++ web/static/js/slave-app.js
@@ -11,12 +11,15 @@ slaveApp.initialize = function () {
     new Chat(channel, sound)
 
     let teamName = $("#team-name")
+    let location = $("#location")
     let timeLeft = $("#time-left")
     let cellsCollected = $("#cells-collected")
 
     teamName.append("Team: " + player.team())
     timeLeft.append("Time: 10 mins")
     cellsCollected.append("Energy Cells: 1 of 5 found")
+
+    $(document.body).css('background-color', player.team());
 
     tracking.start((position) => {
       channel.push('location', {
@@ -27,6 +30,7 @@ slaveApp.initialize = function () {
           lng: position.coords.longitude
         }
       });
+      location = `${position.coords.latitude} ${position.coords.longitude}`
     });
   });
 };
