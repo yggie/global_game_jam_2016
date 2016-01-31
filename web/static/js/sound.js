2a9e01f4c46f521580af1be5f06b15e9ae188e1d
Keith Salisbury <keith.salisbury@alliants.com>
2016-01-31 15:59:16 +0000

    updated sounds

--- web/static/js/sound.js
+++ web/static/js/sound.js
@@ -1,16 +1,18 @@
 export class Sound {
   constructor() {
-
     this.beep = new Howl({
-      src: ["sounds/beep3.wav", "sounds/beep3.ogg"]
+      src: ["sounds/beep3.wav", "sounds/beep3.ogg", "sounds/beep3.mp3"]
     });
     this.laser = new Howl({
-      src: ["sounds/laser-01.wav", "sounds/laser-01.ogg"]
+      src: ["sounds/laser-01.wav", "sounds/laser-01.ogg", "sounds/laser-01.mp3"]
     });
-    this.button = new Howl({src: ["sounds/button-09.wav"]});
   }
 
   new_message() {
     this.laser.play();
+  }
+
+  power_cell() {
+    this.beep.play();
   }
 }
