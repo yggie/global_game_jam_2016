023c9d9aa2384d72631c2986f41bdee9d89ab8c0
Keith Salisbury <keith.salisbury@alliants.com>
2016-01-31 15:47:09 +0000

    update beep sounds

--- web/static/js/sound.js
+++ web/static/js/sound.js
@@ -2,7 +2,7 @@ export class Sound {
   constructor() {
 
     this.beep = new Howl({
-      src: ["sounds/beep.wav"]
+      src: ["sounds/beep3.wav", "sounds/beep3.ogg"]
     });
     this.laser = new Howl({
       src: ["sounds/laser-01.wav", "sounds/laser-01.ogg"]
